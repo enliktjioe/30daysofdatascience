33fb24786aada0b5673c50760aad316796d19cd7
Enlik <enlik@ut.ee>
2021-01-11 21:35:34 +0200

    Finished day 9-11 Apache Kafka

---
+++ week2_datastreaming/script/day9_10_11_kafka.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## References
+# 
+# - https://github.com/mtpatter/postgres-kafka-demo
+# - https://postgresconf.org/system/events/document/000/000/115/Data_pipelines_with_PostgreSQL___Kafka_-_PostgresConf_US_2018_-_Oskari_Saarenmaa.pdf
+# - https://highalpha.com/data-stream-processing-for-newbies-with-kafka-ksql-and-postgres/
+# - https://timber.io/blog/hello-world-in-kafka-using-python/
+# - http://wurstmeister.github.io/kafka-docker/
+
+# ## workspace
+# 
+# Check in `/week2_datastreaming/kafka-docker` and follow `README.md` for instruction with Docker
+# 
+# tutorial from: https://medium.com/big-data-engineering/hello-kafka-world-the-complete-guide-to-kafka-with-docker-and-python-f788e2588cfc
+
+
+
