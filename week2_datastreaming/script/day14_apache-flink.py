026b88de73f8612597fdce89a4b407e7a93c3ee0
Enlik <enlik@ut.ee>
2021-01-12 00:38:02 +0200

    Finished day 14  - apache flink

---
+++ week2_datastreaming/script/day14_apache-flink.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## References
+# 
+# - https://ci.apache.org/projects/flink/flink-docs-stable/dev/python/table_api_tutorial.html
+# - https://ci.apache.org/projects/flink/flink-docs-release-1.12/deployment/cli.html#submitting-pyflink-jobs
+# 
+# ### Installation
+# - https://ci.apache.org/projects/flink/flink-docs-release-1.12/try-flink/local_installation.html
+# 
+# ### Dashboard UI
+# - http://localhost:8081
+# 
+# ![Screenshot_2021-01-12%20Apache%20Flink%20Web%20Dashboard.png](attachment:Screenshot_2021-01-12%20Apache%20Flink%20Web%20Dashboard.png)
+
+# ### example running example python code
+# 
+# ![Screenshot%20from%202021-01-12%2000-31-28.png](attachment:Screenshot%20from%202021-01-12%2000-31-28.png)
+
+
+
