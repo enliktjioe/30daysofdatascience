794f1705fda54ff4dc70845f7f6da8a4fd6dee44
Enlik <enlik@ut.ee>
2021-01-11 22:56:08 +0200

    Finish configure apache storm on ubuntu 20.04
    Install java - zookeeper - apache storm

---
+++ week2_datastreaming/script/day13_apache-storm.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## References
+# 
+# - asda
+# 
+# Installation:
+# 
+# - https://www.tutorialspoint.com/apache_storm/apache_storm_installation.htm
+
+# ## Troubleshooting
+# ### Zookeeper can't be started
+# 
+# - run this `sudo bin/zkServer.sh start /opt/apache-zookeeper-3.6.2-bin/conf/zoo.cfg`, source: https://stackoverflow.com/a/31200219
+# 
+# ### Storm UI can't be started
+# - add `ui.port: 8081` to `conf/storm.yaml`, 
+
+
+
