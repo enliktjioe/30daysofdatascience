6da622b7c3195e0d7e7476034dd65f9a05747cd2
Enlik - <enlik@ut.ee>
2021-01-18 20:41:30 +0200

    Finished day 17 - Seaborn

--- week3_dataviz/script/day17_seaborn.py
+++ week3_dataviz/script/day17_seaborn.py
@@ -275,5 +275,123 @@ df = pd.read_csv("data/Pokemon.csv", encoding= 'unicode_escape') #solution = htt
 df
 
 
+# ### Overlaying Plot
+
+# Setup Color Palletes
+pkmn_type_colors = ['#78C850',  # Grass
+                    '#F08030',  # Fire
+                    '#6890F0',  # Water
+                    '#A8B820',  # Bug
+                    '#A8A878',  # Normal
+                    '#A040A0',  # Poison
+                    '#F8D030',  # Electric
+                    '#E0C068',  # Ground
+                    '#EE99AC',  # Fairy
+                    '#C03028',  # Fighting
+                    '#F85888',  # Psychic
+                    '#B8A038',  # Rock
+                    '#705898',  # Ghost
+                    '#98D8D8',  # Ice
+                    '#7038F8',  # Dragon
+                   ]
+
+
+# Set figure size with matplotlib
+plt.figure(figsize=(10,6))
+ 
+# Create plot
+sns.violinplot(x='Type 1',
+               y='Attack', 
+               data=df, 
+               inner=None, # Remove the bars inside the violins
+               palette=pkmn_type_colors)
+ 
+sns.swarmplot(x='Type 1', 
+              y='Attack', 
+              data=df, 
+              color='k', # Make points black
+              alpha=0.7) # and slightly transparent
+ 
+# Set title with matplotlib
+plt.title('Attack by Type')
+
+
+# ### Swarm Plot
+
+# Swarm plot with Pokemon color palette
+sns.swarmplot(x='Type 1', y='Attack', data=df, 
+              palette=pkmn_type_colors)
+
+
+# Melt DataFrame
+# Pre-format DataFrame
+stats_df = df.drop(['Total', 'Stage', 'Legendary'], axis=1)
+
+melted_df = pd.melt(stats_df, 
+                    id_vars=["Name", "Type 1", "Type 2"], # Variables to keep
+                    var_name="Stat") # Name of melted variable
+melted_df.head()
+
+
+# Swarmplot with melted_df
+sns.swarmplot(x='Stat', y='value', data=melted_df, 
+              hue='Type 1')
+
+
+# 1. Enlarge the plot
+plt.figure(figsize=(10,6))
+ 
+sns.swarmplot(x='Stat', 
+              y='value', 
+              data=melted_df, 
+              hue='Type 1', 
+              split=True, # 2. Separate points by hue
+              palette=pkmn_type_colors) # 3. Use Pokemon palette
+ 
+# 4. Adjust the y-axis
+plt.ylim(0, 260)
+ 
+# 5. Place legend to the right
+plt.legend(bbox_to_anchor=(1, 1), loc=2)
+
+
+# ### Bar Plot
+
+# Count Plot (a.k.a. Bar Plot)
+sns.countplot(x='Type 1', data=df, palette=pkmn_type_colors)
+ 
+# Rotate x-labels
+plt.xticks(rotation=-45)
+
+
+# ### Factor Plot
+
+# Factor Plot
+g = sns.factorplot(x='Type 1', 
+                   y='Attack', 
+                   data=df, 
+                   hue='Stage',  # Color by stage
+                   col='Stage',  # Separate by stage
+                   kind='swarm') # Swarmplot
+ 
+# Rotate x-axis labels
+g.set_xticklabels(rotation=-45)
+ 
+# Doesn't work because only rotates last plot
+# plt.xticks(rotation=-45)
+
+
+# ### Density Plot
+
+# Density Plot
+sns.kdeplot(df.Attack, df.Defense)
+
+
+# ### Joint-Distribution Plot
+
+# Joint Distribution Plot
+sns.jointplot(x='Attack', y='Defense', data=df)
+
+
 
 
