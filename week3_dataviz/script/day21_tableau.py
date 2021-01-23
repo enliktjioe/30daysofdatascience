b870d28ca4ea446fff5fe9844ffec3005da99644
Enlik - <enlik@ut.ee>
2021-01-24 00:42:54 +0200

    Finished day 21 Tableau

---
+++ week3_dataviz/script/day21_tableau.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## References
+# 
+# - [1] https://www.udemy.com/course/kickstarting-tableau/ (FREE)
+# - [2] https://www.udemy.com/course/tableau-desktop-10-core-skills-for-business-users/ (FREE)
+# - [3] https://classroom.udacity.com/courses/ud1006 (Udacity - Data Viz Using Tableau)
+# - [4] https://www.tableau.com/learn/training/20204 (Official Tableau Page)
+
+# ## Tableau Free License For Students
+# 
+# https://www.tableau.com/academic/students
+
+# ## Tableau Sample Viz
+# 
+# Using tutorial files from [4]
+
+# Running superset using docker-compose
+from IPython.display import Image
+Image(filename='img/tableau_sample.png')
+
+
+
+
