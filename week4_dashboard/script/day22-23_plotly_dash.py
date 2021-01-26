4a782aebdeee00d49b24beee5dcf8e572d43e9ca
Enlik - <enlik@ut.ee>
2021-01-26 22:40:42 +0200

    Hello Django

---
+++ week4_dashboard/script/day22-23_plotly_dash.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Reference
+# 
+# - [1] https://realpython.com/python-dash/
+# - [2] https://github.com/realpython/materials
+
+# ## Plotly Dash
+# - 3 Technologies
+#  - Flask, web server
+#  - React.js, UI
+#  - Plotly.js, data visualization
+#  
+# - Plotly built and developed by Canada-based company
+
+# ## General Info
+# 
+# - dataset from https://www.kaggle.com/neuromusic/avocado-prices
+# - run `python app.py` in directory `./avocado_analytics/`
+#     - `app.py` contains the main code for showing dash
+
+# Run in termina
+from IPython.display import Image
+Image(filename='img/dash_run_in_terminal.png')
+
+
+# Simple Running Dash
+from IPython.display import Image
+Image(filename='img/dash_sample.png')
+
+
+## with interactivity
+# Simple Running Dash
+from IPython.display import Image
+Image(filename='img/dash_interactive.png')
+
+
+# ## Deployment Options
+# 
+# - https://flask.palletsprojects.com/en/1.1.x/deploying/
+#     - Heroku
+#     - Google App Engine
+#     - AWS Elastic Beanstalk
+#     - Azure (IIS)
+#     - PythonAnywhere
+
+
+
---
+++ week4_dashboard/script/day24-25_Django.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## References
+# 
+# - [1] https://docs.djangoproject.com/en/3.1/intro/tutorial01/
+# 
+
+# ## Installation
+# 
+# https://docs.djangoproject.com/en/3.1/intro/install/
+
+# ## Project
+# 
+# All Django related project files are stored in `~/week4_dashboard/django_sandbox`
+
+# Check installedd Django version
+get_ipython().system('python -m django --version')
+
+
+# ## Hello Django
+
+# Simple Running Dash
+from IPython.display import Image
+Image(filename='img/django_hello_world.png')
+
+
+
+
