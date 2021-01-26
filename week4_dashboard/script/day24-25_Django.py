b58b4e62126b75ce1cb7ad2ce510455d1ac52778
Enlik - <enlik@ut.ee>
2021-01-26 23:20:33 +0200

    Finished day 24-25 Django

--- week4_dashboard/script/day24-25_Django.py
+++ week4_dashboard/script/day24-25_Django.py
@@ -4,11 +4,19 @@
 # ## References
 # 
 # - [1] https://docs.djangoproject.com/en/3.1/intro/tutorial01/
-# 
+# - [2] https://www.djangoproject.com/start/
+# - [3] https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Tutorial_local_library_website
+# - [4] https://realpython.com/get-started-with-django-1/
 
 # ## Installation
 # 
 # https://docs.djangoproject.com/en/3.1/intro/install/
+
+# ## Why Django [4]
+# 
+# - It' written in Python, one of the most readable and beginner-friendly programming language
+# - Don't need to rely on any external libraries of packages, only one framework needed to build a website
+# - Fantastic [documentation](https://www.djangoproject.com/) and [community support](https://stackoverflow.com/questions/tagged/django)
 
 # ## Project
 # 
@@ -30,6 +38,13 @@ Image(filename='img/django_hello_world.png')
 # - Default Django port is 8000, we can change into different port using this command:
 # 
 # `python manage.py runserver 8080`
+
+# ## Admin page
+
+# Simple Running Dash
+from IPython.display import Image
+Image(filename='img/django_admin.png')
+
 
 
 
